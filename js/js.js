8b87bb66ab0512795f4739cd0ec8f0d16830302a
shiwange <819180342@qq.com>
2017-06-16 14:35:06 +0800

    11
    11

---
+++ js/js.js
@@ -0,0 +1,44 @@
+// JavaScript Document
+
+function yishi()
+{
+	var odiv=document.getElementById('xkzx_ys');
+	
+	if (odiv.style.display=='block')
+	{
+		odiv.style.display='none';	
+	}	
+	else
+	{
+		odiv.style.display='block';	
+	}
+}
+
+function weisheng()
+{
+	var odiv=document.getElementById('xkzx_ws');
+	
+	if (odiv.style.display=='block')
+	{
+		odiv.style.display='none';	
+	}	
+	else
+	{
+		odiv.style.display='block';	
+	}
+}
+
+function yaoshi()
+{
+	var odiv=document.getElementById('xkzx_yaoshi');
+	
+	if (odiv.style.display=='block')
+	{
+		odiv.style.display='none';	
+	}	
+	else
+	{
+		odiv.style.display='block';	
+	}
+}
+
